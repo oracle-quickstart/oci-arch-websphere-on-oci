f94d923b64bd668e0818b8b6385dfcad19150b1d
Orlando Gentil <orlando.gentil@gmail.com>
2020-08-21 07:33:57 -0500

    first comit

--- fss.tf
+++ fss.tf
@@ -1,3 +1,6 @@
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
 resource "oci_file_storage_file_system" "WS_file_system" {
     availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[0]["name"]
     compartment_id = var.compartment_ocid
